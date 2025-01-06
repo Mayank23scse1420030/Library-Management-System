bcf8e1d99475068c97f69fc5fd94528ba4aaae2e
Mayank23scse1420030 <chauhanmayank810@gmail.com>
2025-01-06 14:03:52 +0530

    Create BookController.java

---
+++ BookController.java
@@ -0,0 +1,17 @@
+@RestController
+@RequestMapping("/books")
+public class BookController {
+
+    @Autowired
+    private BookService bookService;
+
+    @GetMapping
+    public List<Book> getAllBooks() {
+        return bookService.getAllBooks();
+    }
+
+    @PostMapping
+    public Book addBook(@RequestBody Book book) {
+        return bookService.addBook(book);
+    }
+}
